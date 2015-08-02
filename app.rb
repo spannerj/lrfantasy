5d38abb4c5a415e9a6314dbda4196137a2180f5a
spannerj1 <spencer.jago.betting@gmail.com>
2015-08-02 22:08:17 +0000

    added more sql

--- app.rb
+++ app.rb
@@ -31,33 +31,29 @@ class Footy < Sinatra::Base
 		end
 	end
 	
-	get '/test' do
-		@players = Player.find_by_sql("	SELECT code, array_agg(ROW(week, total)) AS week_array
-										FROM (select code, week, sum(points) as total
-										  from scores
-										  where code = '4001'
-										  group by code, week
-										  ORDER BY week) AS row
-										GROUP BY code
-										ORDER BY code ")
+	# get '/test' do
+	# 	@players = Player.find_by_sql("	SELECT code, array_agg(ROW(week, total)) AS week_array
+	# 									FROM (select code, week, sum(points) as total
+	# 									  from scores
+	# 									  where code = '4001'
+	# 									  group by code, week
+	# 									  ORDER BY week) AS row
+	# 									GROUP BY code
+	# 									ORDER BY code ")
 
-		p @players.week_array								
-		puts @players.week_array[0]	
-		erb :'players/all'
-	end	
+	# 	p @players.week_array								
+	# 	puts @players.week_array[0]	
+	# 	erb :'players/all'
+	# end	
 
 	get '/' do
 		@total = session['total']
 		@started = session['started']
-		#@players = Player.order('substr(code,1,1)', value: :desc)
+		@players = Player.order('substr(code,1,1)', value: :desc)
 		@weeks = Score.maximum(:week)
-		@players = Player.find_by_sql('	SELECT code, array_agg(ROW(week, total)) AS week_array
-										FROM (select code, week, sum(points) as total
-										  from scores
-										  group by code, week
-										  ORDER BY week) AS row
-										GROUP BY code
-										ORDER BY code')
+		@scores = Score.group(:week).order(:week).select(code, week,sum(points)) 
+		p @weeks
+		p @scores
 		erb :'players/all'
 	end
 
@@ -74,7 +70,7 @@ class Footy < Sinatra::Base
 		Thread.new do
 			insert_player
 		end
-		flash[:notice] = 'Scraping has started, you will see a progress bar soon. Get a coffee as it takes about 25 minutes!'
+		flash[:notice] = 'Scraping has started. Get a coffee as it takes about 25 minutes!'
 	  redirect '/'
 	end
 
